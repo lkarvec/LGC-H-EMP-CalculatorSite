278426a78dc746fe5335aeefda4251406017d8d6
lkarvec <lkarvec@gmail.com>
2021-08-04 12:22:55 -0500

    Ignoring a test file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import './App.css';
 import Inputs from './components/inputs'
 import Graph from './components/graph'
+import utilities from '../misc/utilities';
 //import Button from './components/button' not used
 /*
 TODO: Make ParseIter function for EField Values at Target
@@ -15,36 +16,94 @@ class App extends Component {
         /*
         val is default value given by Henry, 
         variable is the variable name in the equation to be displayed by the GUI
+        This is an array of 13 OBJECTS
+        ID is how to access them and is purely referential
         */
-        {id: 1, val: 0, variable: 'x', variableName: 'x'},
-        {id: 2, val: 0, variable: 'y', variableName: 'y'},
-        {id: 3, val: 0, variable: 'z', variableName: 'z'},
-        {id: 4, val: 70, variable: 'Height of Burst', variableName: 'HOB'},
-        {id: 5, val: 0, variable: 'Gamma', variableName: 'G'},
-        {id: 6, val: 0, variable: 'Magnitude of Magnetic Field', variableName: 'MAG'},
-        {id: 7, val: 20, variable: 'Dip Angle of Magnetic Field', variableName: 'DIP'},
-        {id: 8, val: 50, variable: 'NDELR', variableName: 'NDELR'},
-        {id: 9, val: 10, variable: 'ITERATIONS', variableName: 'ITER'},
-        {id: 10, val: 0, variable: 'A', variableName: 'A'},
-        {id: 11, val: 0, variable: 'B', variableName: 'B'},
-        {id: 12, val: 0, variable: 'RNP', variableName: 'RNP'},
-        {id: 13, val: 0, variable: 'TOP', variableName: 'TOP'}
+        {id: 0, val: 0, variable: 'x', variableName: 'x'},
+        {id: 1, val: 0, variable: 'y', variableName: 'y'},
+        {id: 2, val: 0, variable: 'z', variableName: 'z'},
+        {id: 3, val: 70, variable: 'Height of Burst', variableName: 'HOB'},
+        {id: 4, val: 0, variable: 'Gamma', variableName: 'G'},
+        {id: 5, val: 0, variable: 'Magnitude of Magnetic Field', variableName: 'MAG'},
+        {id: 6, val: 20, variable: 'Dip Angle of Magnetic Field', variableName: 'DIP'},
+        {id: 7, val: 50, variable: 'NDELR', variableName: 'NDELR'},
+        {id: 8, val: 10, variable: 'ITERATIONS', variableName: 'ITER'},
+        {id: 9, val: 0, variable: 'A for inverse shakes', variableName: 'A'},
+        {id: 10, val: 0, variable: 'B for inverse shakes', variableName: 'B'},
+        {id: 11, val: 0, variable: 'N for shakes', variableName: 'RNP'},
+        {id: 12, val: 0, variable: 'T0 for shakes', variableName: 'TOP'}
     ],
     data: [
 
     ],
-    output: {output:""}
+    output: {output:""},
+    error: []
  }
-
+/*curl -X 'POST' \
+  'https://slim1.fulgentcorp.com:14500/emp/calc' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "x": 0,
+  "y": 0,
+  "z": 0,
+  "hob": 0,
+  "gamyld": 0,
+  "bfield": 0,
+  "bangle": 0,
+  "ndelr": 0,
+  "oux": 0,
+  "iter": 0,
+  "ap": 0,
+  "bp": 0,
+  "rnp": 0,
+  "top": 0
+}*/
+//Send a request to DocRobs Fulgent server with the params described above
+// === fetch generated from https://kigiri.github.io/fetch/ and changed to fit our project.  Very useful tool!
  handleInputs = () => {
-  console.log('Submit clicked');
+//add input checking
+  try{
+    utilities.inputCheck(this.state.inputs);
+  }catch(error){
+    console.error(error);
+    this.setState({output:error})
+    break;
+  }
+  
+  let bodyInputs = utilities.formatBody(this.state.inputs);
 
+  console.log('Clicked the button');
+  
+  const data = fetch('https://slim1.fulgentcorp.com:14500/emp/calc', {
+  body: bodyInputs,
+  headers: {
+    'Accept': "application/json",
+    'Content-Type': "application/json"
+  },
+  method: 'POST'
+})
+  .then(res => res.json())
+  .then(
+    (result) => {
+      this.setState({
+        data: JSON.parse(result.message)
+      });
+      this.regexForOutput;
+    },
+    //error catch here so we can bug catch from the fetch and get more descriptive error checking.
+    (error) => {
+      this.setState({
+       error
+      });
+    }
+    )
 
-  //this.setState({ output })
-  //graph.setState({});
 };
 
-
+regexForOutput = () => {
+  this.setState({output: utilities.skim(data)})
+};
 
  render() {
   return (
