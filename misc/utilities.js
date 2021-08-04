c05836e10536fc675bbd13ba4774f87e2921f48e
lkarvec <lkarvec@gmail.com>
2021-08-04 12:22:04 -0500

    Added the inputcheck and formatBody
    InputCheck is an input validator (NOT USING JOI YET, FUTURE ADD)
    FormatBody puts our inputs into a json for the fetch request by passing in the inputs object from the state.

--- misc/utilities.js
+++ misc/utilities.js
@@ -13,6 +13,59 @@ let logger = null;
 
 module.exports = {
 
+	inputCheck : (inputs) =>{
+		let returnMesg = ""
+		//Check x input *** check X Y Z Later, Talk to Henry
+		//if (Math.sign(inputs[0].val) == 1){
+			//}
+		// Height of burst needs to be 60+ Kilometers
+		if (!(60 <= inputs[3].val)){
+			returnMesg += "\n Height of Burst (HOB) needs to be more than 60 Kilometers\n"
+			}
+		//Gamma Yield needs to be below 1 Kiloton
+		if (!(inputs[4].val <= 1)){
+			returnMesg += "\n Gamma Yield (G) needs to be below 1 Kiloton\n"
+			}
+			//Dip angle needs to be -90 - 90
+		if (!(-90 <= inputs[6].val >= 90)){
+			returnMesg += "\n Dip Angle (DIP - https://en.wikipedia.org/wiki/Magnetic_dip) needs to be between -90 and 90\n"
+			}
+		if (!(50 <= inputs[7].val <= 500) && !(Integer.parseInt(inputs[8].val)) ){
+			returnMesg += "\n NDELR needs to be an int between 50 and 500\n"
+		}
+		// Make sure the iterations is an int between 10-100
+		if (!(10 <= inputs[8].val <= 100) && !(Integer.parseInt(inputs[8].val)) ){
+			returnMesg += "\n Iterations (ITER) needs to be an int between 10 and 100\n"
+		}
+		//if return isn't empty, throw it as an error for the error handler to catch
+		if (returnMesg != ""){
+			throw returnMesg
+		}
+		// Fall out of function
+
+	},
+
+	formatBody : (inputs) => {
+		let JSONOBJ = JSON.stringify({"x": inputs[0].val,             
+									"y": inputs[1].val,
+									"z": inputs[2].val,
+									"hob": inputs[3].val,
+									"gamyld": inputs[4].val,
+									"bfield": inputs[5].val,
+									"bangle": inputs[6].val,
+									"ndelr": inputs[7].val,
+									"oux": 0,
+									"iter": inputs[8].val,
+									"ap": inputs[9].val,
+									"bp": inputs[10].val,
+									"rnp": inputs[11].val,
+									"top": inputs[12].val});
+		return JSONOBJ;
+	},
+
+
+
+
 	skim : (message) => {
 		//(I = (\d)+ : (\d+\.*\d*))  Regex for the skimmer
 		let y1 = [];
